c0a1313e98ac199683eef1ab789497a7d6f3a29b
Yonah125 <50438152+Yonah125@users.noreply.github.com>
2019-05-30 12:38:12 +0200

    Update listn.java

--- src/Interface/listn.java
+++ src/Interface/listn.java
@@ -3,11 +3,22 @@ package Interface;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import Objet_base.Rectangle;
+import Objet_base.Objet_Geometrique;
+import P2D.Point2D;
+
 
 public class listn implements ActionListener {
-
+	
 	private Cadre ca;
+	File file = new File("save.txt");
 
+    PrintWriter printWriter = null;
 
 
 	public listn(Cadre c) {
@@ -63,6 +74,56 @@ public class listn implements ActionListener {
 		if(source==ca.getB_poin() ) {
 			ca.setMode(14);
 		}
-}
+		if(source==ca.getB_save()) {
+
+
+	        try
+	        {
+	            printWriter = new PrintWriter(file);
+	           for(int i=0;i<ca.getArr().size();i++) {
+	        	   printWriter.println(ca.getArr().get(i));
+	           }
+	        }
+	        catch (FileNotFoundException e)
+	        {
+	            e.printStackTrace();
+	        }
+	        finally
+	        {
+	            if ( printWriter != null ) 
+	            {
+	                printWriter.close();
+	            }
+	        }
+		}
+		if(source==ca.getB_charge()) {
+			 try
+		        {
+				 Scanner sc=new Scanner(file);
+		         int id = sc.nextInt();
+		         if(id==2) {
+		        	 
+		        	 ca.getArr().add(new Rectangle(new Point2D(sc.nextInt(),sc.nextInt()),new Point2D(sc.nextInt(),sc.nextInt()),new Point2D(sc.nextInt(),sc.nextInt()),new Point2D(sc.nextInt(),sc.nextInt())));
+		         }
+		        }
+			 catch (FileNotFoundException e)
+		        {
+		            e.printStackTrace();
+		        }
+		        finally
+		        {
+		            if ( printWriter != null ) 
+		            {
+		                printWriter.close();
+		            }
+		        }
+			 ca.repaint();
+			 ca.updateJTree();
+			
+
 	
 }
+	}
+}
+	
+
