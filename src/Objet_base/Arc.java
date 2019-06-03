4d2e105878e1db680ac10b877ed9cc4495846088
Yonah125 <50438152+Yonah125@users.noreply.github.com>
2019-06-03 18:03:43 +0200

    Update Arc.java

--- src/Objet_base/Arc.java
+++ src/Objet_base/Arc.java
@@ -12,12 +12,14 @@ public class Arc implements Objet_Geometrique {
 	private int haut;
 	private int larg;
 	private double angle;
+
 	
 	public Arc(Point2D p1, int haut, int larg,double angle) {
 		P1 = p1;
 		this.haut = haut;
 		this.larg = larg;
 		this.angle=angle;
+	
 	}
 
 	@Override
@@ -52,8 +54,54 @@ public class Arc implements Objet_Geometrique {
 
 	@Override
 	public DefaultMutableTreeNode getNode() {
-		// TODO Auto-generated method stub
+		DefaultMutableTreeNode node = new DefaultMutableTreeNode("Arc");
+		node.add(this.getP1().getNode("P1"));
+		DefaultMutableTreeNode n =   (new DefaultMutableTreeNode ("Hauteur"));
+		n.add((new DefaultMutableTreeNode(this.haut)));
+		node.add(n);
+		DefaultMutableTreeNode n1 =   (new DefaultMutableTreeNode ("Largeur"));
+		n.add((new DefaultMutableTreeNode(this.larg)));
+		node.add(n1);
+		DefaultMutableTreeNode n2 =   (new DefaultMutableTreeNode ("Angle"));
+		n.add((new DefaultMutableTreeNode(this.angle)));
+		node.add(n2);
+		
+		
 		return null;
 	}
+
+	public Point2D getP1() {
+		return P1;
+	}
+
+	public void setP1(Point2D p1) {
+		P1 = p1;
+	}
+
+	public int getHaut() {
+		return haut;
+	}
+
+	public void setHaut(int haut) {
+		this.haut = haut;
+	}
+
+	public int getLarg() {
+		return larg;
+	}
+
+	public void setLarg(int larg) {
+		this.larg = larg;
+	}
+
+	public double getAngle() {
+		return angle;
+	}
+
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+	
+	
 
 }
