cfbcd79d14d6ef06f717ed710ecde1be57d2b0db
Yonah125 <50438152+Yonah125@users.noreply.github.com>
2019-06-09 13:42:50 +0200

    Update Rectangle.java

--- src/Objet_base/Rectangle.java
+++ src/Objet_base/Rectangle.java
@@ -162,6 +162,10 @@ public DefaultMutableTreeNode getNode(String nom) {
 public String toString() {
 	return "2 "+this.getPOrigine().getX()+" "+this.getPOrigine().getY()+" "+this.getP1().getX()+" "+this.getP1().getY()+" "+this.getP2().getX()+" "+this.getP2().getY()+" "+this.p3.getX()+" "+this.p3.getY();
 }
+	
+public String toString2() {
+	return this.getPOrigine().getX()+" "+this.getPOrigine().getY()+" "+this.getP1().getX()+" "+this.getP1().getY()+" "+this.getP2().getX()+" "+this.getP2().getY()+" "+this.p3.getX()+" "+this.p3.getY()+" ";
+}
 
 
 
